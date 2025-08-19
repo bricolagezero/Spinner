54580b27665a47dcf457bdcb56357cdf3e7b7528
bricolagezero <129473359+bricolagezero@users.noreply.github.com>
2025-08-19 09:01:46 -0400

    Updates

--- spinner-game/frontend/src/main.tsx
+++ spinner-game/frontend/src/main.tsx
@@ -22,13 +22,3 @@ style.setAttribute("data-inline-global", "true");
 style.textContent = globalStyles;
 document.head.appendChild(style);
 // --------------------------------------------------------------
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/spinner">
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </BrowserRouter>
-  </React.StrictMode>
-);
